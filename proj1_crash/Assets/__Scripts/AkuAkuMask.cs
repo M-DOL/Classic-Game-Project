17d3cf0af75606cb38f785b9f62396ff25062fbc
Wesley Howell <wjhowell@umich.edu>
2016-01-16 15:12:25 -0500

    extra life , aku aku mask

---
+++ proj1_crash/Assets/__Scripts/AkuAkuMask.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AkuAkuMask : MonoBehaviour {
+	public bool follow = false;
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag == "Crash"){
+			follow = true;
+		}
+	}
+
+	void Update () {
+		if (follow) {
+			Vector3 pos = this.transform.position;
+			pos.x = Crash.S.transform.position.x + 0.5f;
+			pos.y = Crash.S.transform.position.y + 2f;
+			pos.z = Crash.S.transform.position.z - 3f;
+			this.transform.position = pos;
+		}
+	}
+}
---
+++ proj1_crash/Assets/__Scripts/ExtraLife.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtraLife : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag == "Crash"){
+			Display.S.IncrementLives ();
+			Destroy (gameObject);
+		}
+	}
+}
