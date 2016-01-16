1e4f2f4fb6906fa153b0004389d422aecfc312b2
Wesley Howell <wjhowell@umich.edu>
2016-01-15 20:51:14 -0500

    Completed Crash Tutorial
    "Force Text" and "Visible Meta Files" should be enabled in "Asset
    Serialization" and "Version Control" for proper merge resolution.

---
+++ proj1_crash/Assets/__Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public float followDistance = 7.5f;
+
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = Camera.main.transform.position;
+		pos.z = Crash.S.transform.position.z - followDistance;
+		Camera.main.transform.position = pos;
+	}
+}
