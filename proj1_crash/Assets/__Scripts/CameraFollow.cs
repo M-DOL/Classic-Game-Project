d14699c144f9f60ce13d51fc7f41c4c003218835
M-DOL <mjdolan@umich.edu>
2016-01-20 01:13:01 -0500

    Fixed CameraFollow.

--- proj1_crash/Assets/__Scripts/CameraFollow.cs
+++ proj1_crash/Assets/__Scripts/CameraFollow.cs
@@ -1,14 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraFollow : MonoBehaviour {
+public class CameraFollow : MonoBehaviour
+{
     public float frontFollowDistance = 4.8f;
-	public float backFollowDistance = 7.5f;
+    public float backFollowDistance = 7.5f;
     public bool frontFacing = true;
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        Vector3 pos = Camera.main.transform.position;
+    public float speed = 2f;
+    public float distThres = .01f;
+    Vector3 newPos;
+    void Start()
+    {
+        
+        newPos = Camera.main.transform.position;
+        newPos.z = Crash.S.transform.position.z - frontFollowDistance;
+        Camera.main.transform.position = newPos;
+    }
+    void Update()
+    {
+        newPos = Camera.main.transform.position;
+        newPos.z = Crash.S.rigid.velocity.z * Time.deltaTime;
         if (Crash.S.rigid.velocity.z > .01f)
         {
             frontFacing = true;
@@ -17,7 +28,14 @@ public class CameraFollow : MonoBehaviour {
         {
             frontFacing = false;
         }
-        pos.z = Crash.S.transform.position.z - (frontFacing ? frontFollowDistance : backFollowDistance);
-        Camera.main.transform.position = pos;
-	}
+        newPos.z = Crash.S.transform.position.z - (frontFacing ? frontFollowDistance : backFollowDistance);
+        if (Mathf.Abs(newPos.z - Camera.main.transform.position.z) > distThres)
+        {
+            Camera.main.transform.position = Vector3.MoveTowards(Camera.main.transform.position, newPos, Time.deltaTime * speed + Mathf.Abs(Crash.S.rigid.velocity.z) / 30f);
+        }
+        else
+        {
+            Camera.main.transform.position = newPos;
+        }
+    }
 }
