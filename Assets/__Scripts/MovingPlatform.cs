f74c81be7dab9086fd891eb20fbbe97c456fc5ee
Wesley Howell <wjhowell@umich.edu>
2016-02-02 01:20:02 -0500

    moving platform

---
+++ Assets/__Scripts/MovingPlatform.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	public float speed = 2f;
+	public int turnSpeed;
+	public int timer;
+	
+	// Update is called once per frame
+	void Update () {
+		this.GetComponent<Rigidbody>().velocity = Vector3.right * speed;
+		timer++;
+		if (timer >= turnSpeed) {
+			speed *= -1;
+			timer = 0;
+		}
+	}
+}
